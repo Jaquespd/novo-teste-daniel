52d46347c42ac19978ff8fee37567b24bc9c6b54
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-07-21 18:12:53 +0100

    Background - not finished

--- frontend/src/components/Geral/index.js
+++ frontend/src/components/Geral/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
 import logo from '../../assets/logo.png';
 
-import { Container } from './styles';
+import { Container, Background } from './styles';
 import Header from '../../components/Header';
 import Main from '../../components/Main';
 
 function Geral() {
   return (
     <Container>
+      <Background />
       <img src={logo} alt="e'Day" /> 
       <Header />
       <Main />
--- frontend/src/components/Geral/styles.js
+++ frontend/src/components/Geral/styles.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
+import background from '../../assets/background.png';
+
 export const Container = styled.div`
   max-width: 400px;
 	margin: 200px auto;
 	color: #f5f5f5;
 	margin-top: 10px;
+`;
+
+export const Background = styled.div`
+	flex: 1;
+	background: url(${background}) no-repeat center;
 `;
